32eebe982f651594f257613b5b181e43f3204c27
MonnPoup <monnetpoupon.lucas@gmail.com>
2021-05-17 12:56:07 +0200

    spacebetween navbar

---
+++ reactapp/src/ScreenHome.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import { Nav, NavItem, NavLink } from "reactstrap";
+import "./App.css";
+
+function Home() {
+  return (
+    <div>
+      <Nav className="navbarHome">
+        <div className="">
+        <h1>DÉCODE.</h1> 
+        </div>
+        <div>
+        <NavItem>
+          <NavLink href="#">
+            <img src="palette.svg" alt="icon palette de couleur" style={{width: "30px"}}/>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            <img src="heart.svg" alt="icon coeur" style={{width: "30px"}}/>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            <img src="user.svg" alt="icon utilisateur" style={{width: "30px"}}/>
+          </NavLink>
+        </NavItem>
+        </div>
+      </Nav>
+      <div>
+      <img src="palette.svg" alt="icon palette de couleur" style={{width: "30px"}}/>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
