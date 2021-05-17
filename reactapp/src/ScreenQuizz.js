fa1a0dc1197b84535034b3147ecdd6a16b53a77c
Maud Jossier <Maudjossier@Air-de-Maud.concorde-pereire.loc>
2021-05-17 14:19:56 +0200

    page quizz

--- reactapp/src/ScreenQuizz.js
+++ reactapp/src/ScreenQuizz.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import navbar from './navbar'
+import App from './App.css'; 
+
+
 
 
 
 function Quizz() {
 
 
-
     return (
-        <div style={{backgroundColor: '#203126', height:'100vh', width:'100%'}}> 
+        <div> 
+         <div className= 'navbar'>
+    <h1 style={{marginLeft: '20px'}}>DÉCODE.</h1> 
+    
+    <div className= 'icon'>  
+    <img src='palette.svg' alt='user icon' style={{width: '30px', margin: '20px'}}/>
+    <img src='heart.svg' alt='heart icon' style={{width: '30px', margin: '20px'}}/>
+    <img src='user.svg' alt='palette icon' style={{width: '30px', margin: '20px'}}/>
+    </div>
         
+
+  </div>
+
+        <div style={{backgroundColor: '#203126', marginTop:'120px', height:'100vh', width:'100%'}}> 
+            <p style={{color:'white', fontSize:'36px'}}> Parmis les styles de décoration suivants lesquels préférez-vous ? </p>
+        </div>
      </div>
     );
   }
--- reactapp/src/navbar.js
+++ reactapp/src/navbar.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import App from './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+
 
 
 
