8aeabaa7edbbb3c6f1e17dea3bdfb2704ad11d6f
Sybil <grainger@Air-de-Grainger.concorde-pereire.loc>
2021-05-17 15:29:51 +0200

    css syb

--- reactapp/src/navbar.js
+++ reactapp/src/navbar.js
@@ -10,7 +10,7 @@ import { Nav, NavItem, NavLink } from "reactstrap";
 function NavBar() {
   return (
     <div className= 'navbar'>
-    <div><h1 style={{marginLeft: '20px'}}>DÉCODE.</h1> </div>
+    <div><h2 style={{marginLeft: '20px'}}>DÉCODE.</h2> </div>
     <div className= 'icon'>  
     <Link to = '/login'><img src='palette.svg' alt='user icon' style={{width: '30px', margin: '20px'}}/></Link>
     <Link to = '/wishlist'><img src='heart.svg' alt='heart icon' style={{width: '30px', margin: '20px'}}/></Link>
