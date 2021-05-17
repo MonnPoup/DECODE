50b2a5fe3a4453bf5ed4e0154e1e5ebbb9faf086
MonnPoup <monnetpoupon.lucas@gmail.com>
2021-05-17 10:59:59 +0200

    création composant navbar home

---
+++ reactapp/src/NavHome.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Nav() {
+    return (
+       <div>
+         <img src="https://static.lacapsule.academy/wks2/burger_1.png" />
+         <h4>nom du burger</h4>
+         <p>description de la recette</p>
+       </div>
+    )
+   }
+   
+   export default Burger;
