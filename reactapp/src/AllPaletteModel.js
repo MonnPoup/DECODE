2e8fab541c15f12be7f7f2ba07f7f0ccd05e4c2e
MonnPoup <monnetpoupon.lucas@gmail.com>
2021-05-21 15:42:08 +0200

    pull

---
+++ reactapp/src/AllPaletteModel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function AllPaletteModel() {
+    return (
+        <div className="containerAllPalettes">
+        <h3 className="h3Container">Hello</h3>
+        <div className="traitContainerAllPalettes"></div>
+        <div className="paletteAllPalettes"></div>
+      </div>
+    )
+}
+
+export default AllPaletteModel;
