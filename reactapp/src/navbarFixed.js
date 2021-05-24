21624d2a71c51edd912770e2438ed1e2e44dcc03
Maud Jossier <Maudjossier@Air-de-Maud.concorde-pereire.loc>
2021-05-24 16:03:28 +0200

    petit détail

--- reactapp/src/navbarFixed.js
+++ reactapp/src/navbarFixed.js
@@ -12,6 +12,7 @@ const text = <span>Mon compte</span>;
 const content = (
   <div>
     <Link style={{color: 'grey', textDecoration: 'underline grey'}} to ='/mypalette'><p>Ma palette</p></Link>
+    <Link style={{color: 'grey', textDecoration: 'underline grey'}} to ='/shoppinglist'><p>Ma shopping list</p></Link>
     <Link style={{color: 'grey', textDecoration: 'underline grey'}} to ='/'><p onClick={() => props.suppressionToken()}>Déconnexion</p></Link>
   </div>
 );
