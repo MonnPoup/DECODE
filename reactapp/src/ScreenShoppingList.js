266db730ff6b241e6fdbf918d52a7ea25587356a
Maud Jossier <Maudjossier@ipad.home>
2021-05-25 21:49:54 +0200

    css remis dans le doc

--- reactapp/src/ScreenShoppingList.js
+++ reactapp/src/ScreenShoppingList.js
@@ -114,7 +114,7 @@ return ( <Redirect to='/' /> )
   
       /////// pop over si pas connecté ////// 
  if (!props.token){
-  var popoverWishList = <Popover placement="bottomRight" content='Veuillez vous connecter pour ajouter un article à votre Wishlist' trigger="hover">
+  var popoverWishList = <Popover placement="bottomRight" content='Veuillez vous connecter pour ajouter un article à votre Wishlist' trigger="click">
     <FontAwesomeIcon style={{cursor:'pointer', width: '15px'}} icon={faHeart}/>
     </Popover>
 } else {
@@ -123,21 +123,21 @@ return ( <Redirect to='/' /> )
   
 
   return ( 
-   <Col key={i} md={2}lg={3} style={{backgroundColor:'white', margin:'10px',  display:'flex', flexDirection:'column', justifyContent:'space-between'}}> 
+   <Col key={i} md={2}lg={3} className="articleCard" > 
     <a href={article.merchantUrl} target="_blank">
-    <div style={{display:'flex', justifyContent:'center', alignItems: 'center', width: '100%', height: '35vh'}} className='productImage' >
+    <div  className='productImage' >
       <img style={{maxWidth:'100%', maxHeight: '100%'}} src={article.imageUrl}  alt='product' /> 
       {/* image + picto coeur  */}
     </div>
     </a>
-    <div className="productInfo" style={{display:'flex', justifyContent:'space-between', margin:"5px 10px 5px 0px"}}> 
-      <div style={{display:'flex', flexDirection:'column', justifyContent:'space-around', marginRight: '5px'}}> 
+    <div className="productInfo" > 
+      <div className="cardPartLeft"> 
         <a href={article.merchantUrl} target="_blank">
           <h5 className='articleCardTitle'> {article.name} </h5> 
         </a>
         <h6 className='articleCardBrand'> {article.brand} </h6>
       </div>
-      <div style={{display:'flex', flexDirection:'column',marginLeft: '10px', margin: '0px', alignItems:'flex-end', justifyContent: 'flex-start'}}> 
+      <div className="cardPartRight" > 
         {popoverWishList}
         <p className='articleCardTitle'> {article.price}€ </p>
       </div>
@@ -159,11 +159,11 @@ return ( <Redirect to='/' /> )
    /// map des inspirations /// 
   var displayInspo = userPalette.inspirations.map((photo, i) => {
     const content = (
-      <img style={{minWidth:'400px', minHeight:'400px', maxWidth:'700px', maxHeight: '700px'}} src={photo} alt='inspo'/>
+      <img className="displayInspo" style={{minWidth:'400px', minHeight:'400px', maxWidth:'600px', maxHeight: '600px'}} src={photo} alt='inspo'/>
       )
     return (
       <Popover content={content} placement='right' >
-      <Col key={i} md={2}lg={3} style={{backgroundColor:'white', margin:'10px', display:'flex'}}>
+      <Col key={i} md={2}lg={3}  style={{backgroundColor:'white', margin:'10px', display:'flex'}}>
         <div  style={{height: '100%',display: 'flex' , justifyContent:'center', alignItems: 'center'}}>
           <img style={{maxWidth:'100%', maxHeight: '100%'}} src={photo} alt='inspo'/>
         </div>
@@ -234,11 +234,11 @@ var content = (
     <div style={{height: '17vh', backgroundColor: '#203126'}}></div>  {/* trait vert */}
 
   {/* CONTAINER ARTICLES */}
-    <div className="ShoppingList" style={{dislpay:'flex', backgroundColor:'#FCFBF6', paddingBottom:'3vh' }}>  
+    <div className="ContainerShoppingList" style={{dislpay:'flex', backgroundColor:'#FCFBF6', paddingBottom:'3vh', justifyContent:'center' }}>  
 
   {/* PALETTE + BOUTON REFAIRE QUIZZ */}
-        <div style={{display:'flex', justifyContent:'space-between', padding:'10px'}}> 
-          <div className ='PaletteColors'style={{display:'flex', justifyContent:'space-around', width:'25%'}}> 
+        <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px'}}> 
+          <div className="PaletteColors"> 
             {displayPalette}
           </div>
           <Link to="/quiz">
@@ -247,24 +247,24 @@ var content = (
         </div>
 
   {/* SELECTION D'ARTICLE */}
-        <div className='Articles' style={{ marginTop:'1%', marginLeft:'10%', marginRight:'10%'}}> 
+        <div className='ArticleShoppingList' > 
           <div className="ShoppingList-Text"> 
-            <h4 style={{fontWeight:'bold', width:'90%', borderBottom:'3px solid #203126', color: '#203126', marginBottom: '10px'}}>
+            <h4  className="TitleShoppingListContainer">
             VOTRE SHOPPING LIST {paletteName}</h4>
 
             {/* FILTRER  */}
             <Popover content={content} placement='bottom' >
-            <Button id="Popover1" type="button" style={{backgroundColor:'#FCFBF6', border:'1px solid black', color:'#203126'}}> 
-              <FontAwesomeIcon style={{cursor:'pointer', width: '15px', paddingRight:'10px'}} icon={faSearch}/>
+            <Button id="Popover1" type="button" > 
+              <FontAwesomeIcon style={{cursor:'pointer', width: '15px'}} icon={faSearch}/>
               Filtrer
             </Button>
             </Popover>
           </div>
   
       {/* SLIDER */}  
-          <div className="scroller " style={{display:'flex', flexDirection:'row', justifyContent: 'center'}}> 
-            <Container lg={12} md={12} style={{ display:'flex', justifyContent: 'center', marginBottom: '3px'}} > 
-              <Row  lg={12} md={12} style={{ display:'flex', justifyContent: 'center'}}> 
+          <div className="scrollerShoppingList " > 
+            <Container className="containerArticles" lg={12} md={12} > 
+              <Row  className="rowArticles" lg={12} md={12}> 
                 {displayArticles}
               </Row>
             </Container> 
@@ -274,23 +274,23 @@ var content = (
 
 
   {/* BOUTTON SCROLL */}
-    <div className="Scroll" style={{backgroundColor:'#203126', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:'6px'}}>
+    <div className="scrollShoppingList" >
       <h5 className='textShoppingList'>Découvrir des photos d'inspiration</h5>
       <a href="#sect2">
-        <img style={{height:"30px", marginTop: '6px'}} src="doubleChevron.svg" alt="double chevron" />
+        <img className="arrowShoppingList" src="doubleChevron.svg" alt="double chevron" />
       </a>
     </div>
 
 
 {/* PARTIE INSPIRATION */}
-    <div id='sect2' style={{backgroundColor: '#fcfbf6', paddingBottom:'3vh'}} >
-      <div className="ShoppingList-Text" style={{marginLeft:'10%', marginRight:'10%'}}> 
-      <h4 style={{fontWeight:'bold', width:'90%', borderBottom:'1px solid #203126', color: '#203126', marginBottom: '15px', paddingTop: '18vh'}}> INSPIRATIONS </h4>
+    <div id='sect2' className="ContainerInspi" >
+      <div className="Inpiration-Text" > 
+      <h4 className="TitleShoppingListContainer" style={{paddingTop:"18vh"}}> INSPIRATIONS </h4>
       </div>
 
-      <div className="Inspirations" style={{backgroundColor:'#FCFBF6', height:'100%', display: 'flex', width: '100%'}}> 
-        <Container lg={12} md={12} style={{ display:'flex', justifyContent: 'center', marginBottom: '3px'}} > 
-            <Row  lg={12} md={12} style={{ display:'flex', justifyContent: 'center'}}> 
+      <div className="Inspirations-DisplayPhoto"> 
+        <Container className='containerPhotoInspi' lg={12} md={12} > 
+            <Row  className='rowPhotoInspi' lg={12} md={12} > 
               {displayInspo}
             </Row>
         </Container>
